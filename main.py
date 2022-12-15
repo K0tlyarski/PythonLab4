05e458aacec3afa742219490db25d632069f0d45
K0tlyarski <djgame2.9.6@gmail.com>
2022-12-15 18:38:53 +0400

    Added a function to create a data frame

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
+
+def create_DataFrame() -> pd.DataFrame:
+    """This function takes data from 2 csv files and create dataframe with 2 columns"""
+    df1 = pd.read_csv(os.path.join("/Users", "vadimkotlarskij", "Desktop", "Python", "Lab4TEST", "annotationtiger.csv"),
+                      sep=',',
+                      header=None)
+    df2 = pd.read_csv(
+        os.path.join("/Users", "vadimkotlarskij", "Desktop", "Python", "Lab4TEST", "annotationleopard.csv"), sep=',',
+        header=None)
+    df = pd.concat([df1, df2], ignore_index=True)
+    df.drop(1, axis=1, inplace=True)
+    df.rename(columns={0: 'AbsolutePath', 2: 'DatasetClass'}, inplace=True)
+    return df
+
